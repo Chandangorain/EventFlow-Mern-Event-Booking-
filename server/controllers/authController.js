028b22db366755e1287030058a5db68760f9280f
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-22 15:11:55 +0530

    registeruser logic complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,10 @@
 const User = require('../models/User');
+const otp=require('../models/otp');
+const bcrypt=require('bcryptjs');
+const { sendOTPEmail } = require('../utils/email');
 
 
-//register user
+//register user controller  : req.body → validate → check user → hash password → generate OTP → save in DB → send email → response
 exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     let userExists = await User.findOne({ email });
@@ -14,17 +17,26 @@ exports.registerUser = async (req, res) => {
     const hashedPassword=await bcrypt.hash(password,salt);      //hashing the pass
 
     try{
-        const user=new User({name,email,password:hashedPassword});
+        const user=new Usercreate({name,email,password:hashedPassword,role:'user',isVerified:false});
         await user.save();
-        res.status(201).json({message:'User registered successfully'})
-
+       
         const otp = Math.floor(100000 + Math.random() * 900000).toString(); // Generate a 6-digit OTP
         console.log(` OTP for ${email}: ${otp}`); 
+         await otp.create({ email, otp, action: 'account_verification' }); // Store OTP in DB 
         await sendOTPEmail(email, otp, 'account_verification'); // Send OTP . messge of account verification from email.js will be used to ssend here to user
 
+       
+        res.status(201).json({ 
+            message: 'User registered successfully. Please check your email for OTP to verify your account.' ,
+            email:user.email
+        });
         
 
     }catch(err){
         res.status(500).json({message:'Error registering user',error:err.message})
     }
-}    
+} 
+
+exports.loginUser=async(req,res)=>{ 
+    
+}
