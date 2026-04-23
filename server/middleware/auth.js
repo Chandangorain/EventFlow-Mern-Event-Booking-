cb4e98d534db8cfe988bfe0c5f5e8242364409cb
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-23 15:55:47 +0530

    admin routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,16 @@
 const jwt=require('jsonwebtoken');
 const User=require('../models/User');
 
+
+// user authentication middleware to protect routes
+
 const protect=async(req,res,next)=>{
     let token=req.headers.authorization && req.headers.authorization.startsWith('Bearer') ? req.headers.authorization.split(' ')[1] : null;
     if(token){
 
        try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
-        req.user=await User.findById(decoded.id).select('-password');
+        req.user=await User.findById(decoded.id).select('-password');  // find user by id and exclude password in database
         
         if(!req.user){
             return res.status(401).json({message:'User not found'});
@@ -22,4 +25,13 @@ const protect=async(req,res,next)=>{
         return res.status(401).json({message:'No token, authorization denied'});
     }
     
+};
+
+// protect routes for admin only
+const admin=(req,res,next)=>{
+    if(req.user && req.user.role==='admin'){  // if role==admin only then next else denied
+        next();
+    }else{
+        return res.status(403).json({message:'Access denied. Admin only.'});
+    }
 }
