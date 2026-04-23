e1c887891f95c73b35221333932cc5fe2db4db47
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-23 16:43:41 +0530

    create new event

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -2,7 +2,7 @@
 const Event = require('../models/Event');
 
 // Get all events
-const getAllEvents = async (req, res) => {
+exports.getAllEvents = async (req, res) => {
     try {
         const filters={};       // applying filter for category and location
         if(req.query.category){
@@ -21,3 +21,17 @@ const getAllEvents = async (req, res) => {
         res.status(500).json({ message: 'Server Error', error: error.message });
     }
 };
+
+// Create a new event
+exports.getEventById = async (req, res) => {
+    try{
+        const event=await Event.findbyId(req.params.id);
+        if(!event){
+            return res.status(404).json({message:'Event not found'});
+        } 
+        res.json(event);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+}
