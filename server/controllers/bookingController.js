d640cc77ade5ad93aeccb92d9174f48d7268387f
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-24 16:50:47 +0530

    existing booking check

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
 const booking=require('../models/bookings');
+const event = require('../models/Event');
 const event=require('../models/Event');
 const OTP=require('../models/otp');
 const {sendEmail,sendBookingEmail} = require('../utils/emailService');  
@@ -18,9 +19,23 @@ exports.sendBookingOTP=async(req,res)=>{  // generate otp->if previous otp exisi
     }
 }
 
+//book event and seat availbility check and otp verification
 exports.bookEvent=async(req,res)=>{
     const otpRecord=await OTP.findOne({email:req.user.email, action:'event_booking'});
     if(!otpRecord ){
         return res.status(400).json({message:'Invalid or expired OTP'});
     }
+
+    const event=await Event.findbyId(eventId);
+    if(!event){
+        return res.status(404).json({message:'Event not found'});
+    }
+    if(event.availableSeats<=0){
+        return res.status(400).json({message:'No seats available'});
+    }
+
+   const existingBooking = await Booking.findOne({ userId: req.user.id, eventId });
+        if (existingBooking && existingBooking.status !== 'cancelled') {
+            return res.status(400).json({ message: 'Already booked or pending' });
+        }
 }
