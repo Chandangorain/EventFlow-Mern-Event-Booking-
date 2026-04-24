65fea6692da5c1dd21a48b519876e8f4eda0544b
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-24 16:43:44 +0530

    bookevent controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -6,7 +6,7 @@ const {sendEmail,sendBookingEmail} = require('../utils/emailService');
 const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();   
 
 
-exports.sendBookingOTP=async(req,res)=>{
+exports.sendBookingOTP=async(req,res)=>{  // generate otp->if previous otp exisist , delete -> generate and send otp
     try {
         const otp = generateOTP();
         await OTP.findOneAndDelete({ email: req.user.email, action: 'event_booking' });
@@ -15,5 +15,12 @@ exports.sendBookingOTP=async(req,res)=>{
         res.json({ message: 'OTP sent successfully' });
     } catch (error) {
         res.status(500).json({ message: 'Error sending OTP', error: error.message });
+    }
+}
+
+exports.bookEvent=async(req,res)=>{
+    const otpRecord=await OTP.findOne({email:req.user.email, action:'event_booking'});
+    if(!otpRecord ){
+        return res.status(400).json({message:'Invalid or expired OTP'});
     }
 }
