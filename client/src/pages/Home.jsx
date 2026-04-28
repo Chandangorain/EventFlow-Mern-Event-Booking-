9f9cc41fcc3496b53196de9e01323206e2e73816
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-28 17:41:47 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,5 @@
-import React,{}
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../utils/axios';
+import { FaCalendarAlt, FaMapMarkerAlt, FaSearch, FaRegClock, FaTicketAlt, FaShieldAlt } from 'react-icons/fa';
+
